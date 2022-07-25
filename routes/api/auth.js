a3c46b80b2f7ced297b8589d60fb9580dc7cd883
Mohamed Faisal Sindhi <102270681+PseudoFaisal@users.noreply.github.com>
2022-07-25 17:19:39 +0530

    Update auth.js

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,4 +1,4 @@
-const express = require('express');
+onst express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const {check, validationResult} = require('express-validator');
